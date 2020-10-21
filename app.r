73f712885d62480740d23ef1da2aa0af4f04fe07
Matteo Spada <pejev2003@me.com>
2020-10-21 16:41:25 +0000

    Generated Weighted Sum page, updated R virtual environment, deleted test.r script

--- app.r
+++ app.r
@@ -1,8 +1,7 @@
 library(shiny)
 library(shinydashboard)
-library(tools)
-library(vroom)
 library(tidyverse)
+library(openxlsx)
 
 # Dashboard global header for the sMCDA tool
 dbHeader <- dashboardHeader(title = "sMCDA Tool",
@@ -12,8 +11,8 @@ dbHeader <- dashboardHeader(title = "sMCDA Tool",
                                     height = "18px")),
                                     class = "dropdown"),
                             tags$li(a(href="mailto:matteo.spada@psi.ch",
-                                      icon("envelope"),
-                                      title = "Contact Us"),
+                                    icon("envelope"),
+                                    title = "Contact Us"),
                                     class = "dropdown"),
                             tags$li(a(href = 'http://www.psi.ch',
                                     icon("power-off"),
@@ -33,8 +32,8 @@ ui <- dashboardPage(
     # Generate the sidebar items
     sidebarMenu(
       menuItem("Data Input", tabName = "input", icon = icon("database")),
-      menuItem("Criteria Selection", tabName = "criteria", icon=icon("brain")),
-      menuItem("Method", tabName = "method", icon = icon("whmcs"),
+      menuItem("Criteria Selection", tabName = "criteria", icon=icon("whmcs")),
+      menuItem("Run sMCDA", tabName = "mcda", icon = icon("chart-bar"),
                menuSubItem("Weighted Sum", tabName = "ws", icon = icon("layer-group")),
                menuSubItem("Outranking Approach", tabName = "outranking", icon = icon("layer-group"))),
       menuItem("About", tabName = "about", icon = icon("question-circle")) 
@@ -62,7 +61,7 @@ ui <- dashboardPage(
                 h1("Criteria Selection"),
                 selectInput(
                   "variables",
-                  multiple = TRUE, 
+                  multiple = TRUE,
                   label = h3("Select Variable"),
                   ""
                 ),
@@ -72,8 +71,19 @@ ui <- dashboardPage(
       ),
       tabItem("ws",
               fluidPage(
-                h1("Weighted Sum")
-              )
+                h1("Weighted Sum"),
+                box(width = 4,
+                    uiOutput("sliders")
+                    ),
+                box(width = 3,
+                    numericInput("MCruns", "Input Monte-Carlo runs", 1),
+                    actionButton("sMCDA","Perform sMCDA")
+                ),
+                box(width = 5,
+                    plotOutput("IndicatorHistogram")
+                )
+                )
+                
       ),
       tabItem("outranking",
               fluidPage(
@@ -96,15 +106,9 @@ server <- function(input, output, session){
     inFile <- input$file
     # Instead # if (is.null(inFile)) ... use "req"
     req(inFile)
-    read.csv(inFile$datapath)
-    # 
-    # ext <- file_ext(input$file$name)
-    # switch(ext,
-    #        csv = vroom(input$file$datapath, delim = ","),
-    #        tsv = vroom(input$file$datapath, delim = "\t"),
-    #        validate("Invalid file; Please upload a .csv or .tsv file")
-    # )
-  }) 
+    read.xlsx(inFile$datapath)
+
+  })
   
   output$datatab <- renderDataTable({
     data()
@@ -117,9 +121,83 @@ server <- function(input, output, session){
       choices=names(data()))
   })
   
-  output$datatab2 <- renderDataTable({
+  crit <- reactive({
     data() %>% select(!!!input$variables)
+  })
+  
+  # critnames <- reactive({
+  #   names(data() %>% select(!!!input$variables))
+  # })
+  
+  alt <- reactive({
+    data() %>% select(1)
+  })
+  
+  output$datatab2 <- renderDataTable({
+    crit()
     })
+ 
+  output$sliders <- renderUI({
+    
+    critnames <- names(crit())
+    numSliders <- dim(crit())[2]
+    lapply(1:numSliders, function(i) {
+      sliderInput(
+        paste0('Criteria', critnames[i]),
+        paste0('Select the Weight (%) for ', critnames[i]),
+        min = 0,
+        max = 100,
+        value = 100/numSliders,
+        step =1)
+    })
+    
+  })
+  
+  observe(
+    updateNumericInput(session, "MCruns", value = input$value)
+  )
+  
+  observeEvent(input$sMCDA, {
+    
+    critnames <- names(crit())
+    numSliders <- dim(crit)[2]
+    tmp1 <- lapply(1:length(alt), function (i) calc_ws(crit[i]))
+    tmp2 <- lapply(1:length(alt), function(i) rep(alt,length(critnames)))
+    tmp3 <- lapply(1:length(alt), function(i) rep(critnames))
+    
+    df.forhistplot <- as.data.frame(cbind(as.numeric(unlist(tmp2)),unlist(tmp1),unlist(tmp3)))
+    colnames(df.forhistplot) <- c("id","normind","indname")
+    
+  })
+  
+  output$IndicatorHistogram_res <- renderPlot({
+    
+    critnames <- names(data() %>% select(!!!input$variables))
+    cbbPalette <- colorBin(
+      palette = brewer.pal(length(critnames),"Spectral"),
+      bins=length(critnames),
+      na.color = c("white")
+    )
+    
+    ggplot(df.forhistplot) +
+      geom_bar(aes(x=as.numeric(levels(id))[id],y=as.numeric(levels(normind))[normind],fill=indname),position="stack",
+               stat="identity", width=0.5) +
+      scale_x_continuous(breaks=c(seq(0,31,1))) +
+      scale_y_continuous(breaks=c(seq(0,1,0.1))) +
+      coord_cartesian(ylim=c(0,1),xlim=c(0,31)) +
+      scale_fill_manual(values=cbbPalette) +
+      xlab("Area") + ylab("sMCDA") +
+      theme_set(theme_bw(base_size = 20)) +
+      theme(panel.border=element_rect(size=1.2, colour = "black"),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            axis.title=element_text(size=22,face="bold"),
+            axis.ticks=element_line(size = 1.2,colour = "black"),
+            legend.title=element_blank(),
+            legend.justification=c(1,1),
+            legend.position="bottom")
+    
+  })
   
 }
 
