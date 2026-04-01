56ff868031e162ff385f172b64fd8282b023aa63
mspada <matteo.spada@zhaw.ch>
2026-04-01 17:48:14 +0200

    Correct issue for shinyapp.io

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,2 +1,7 @@
+if (!exists("dbHeader", inherits = TRUE) ||
+    !exists("dashboardPage", mode = "function", inherits = TRUE)) {
+  source("global.R", local = TRUE)
+}
+
 source("legacy_definitions.R", local = TRUE)
-shinyApp(ui, server)
+shiny::shinyApp(ui, server)
